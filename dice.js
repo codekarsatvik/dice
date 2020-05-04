0185e83e6e4528874541ba1df07e29552a9feb6f
satvik24022 <61509199+satvik24022@users.noreply.github.com>
2020-05-04 19:54:54 +0530

    Update dice.js

--- dice.js
+++ dice.js
@@ -1,8 +1,8 @@
 var random = Math.floor((Math.random()*6 + 1));
 var random1 = Math.floor((Math.random()*6 + 1));
 
-document.querySelector("img").setAttribute("src","C:/Users/om/Desktop/web/DOM/Dicee Challenge - Starting Files/images/dice"+ random +".png");
-document.querySelector("img.img2").setAttribute("src","C:/Users/om/Desktop/web/DOM/Dicee Challenge - Starting Files/images/dice"+ random1 +".png")
+document.querySelector("img").setAttribute("src","dice"+ random +".png");
+document.querySelector("img.img2").setAttribute("src","dice"+ random1 +".png")
 
 if(random>random1)
 {
@@ -20,4 +20,4 @@ else
 
 
 function doSomethingOnClick ()
-{ alert( 'nahi apni baari chal cheater modm !' ); }
+{ alert( 'nahi , apni baari chal cheater modm !' ); }
